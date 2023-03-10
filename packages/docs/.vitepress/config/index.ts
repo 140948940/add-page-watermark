c5fb2ca61b8c3a2c42753f774b2ae717cd9a54c2
A SOUL <59335580+140948940@users.noreply.github.com>
2023-03-10 11:23:07 +0800

    docs:配置文档相关

--- packages/docs/.vitepress/config/index.ts
+++ packages/docs/.vitepress/config/index.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vitepress'
 import { enConfig } from './en'
 import { sharedConfig } from './shared'
 import { zhConfig } from './zh'
+import { fileURLToPath} from 'node:url'
 
 export default defineConfig({
   ...sharedConfig,
-
+  base:'/watermark-page/',
+  vite:{
+    resolve: {
+      alias: {
+        // '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'watermark-page': fileURLToPath(new URL('../../../watermark-page/src', import.meta.url)),
+      },
+    },
+  },
   locales: {
     root: { label: '简体中文', lang: 'zh-CN', link: '/', ...zhConfig },
   },
